f01ac77ef5b1ace698f5b8af29d48f0da739406c
CollinT22 <ct359@njit.edu>
2022-02-08 10:40:37 -0500

    updated my name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Collin"
+        name = "Collin T"
         return render_template('index.html', name=name)
